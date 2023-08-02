2cf273fec4cfa9a52766ed762635749572379344
giovannadso <127310419+giovannadso@users.noreply.github.com>
2023-08-02 11:52:49 -0300

    Add files via upload

---
+++ dog.js
@@ -0,0 +1,49 @@
+// Questão 01//
+
+let myAge = 17; // Minha idade em anos humanos.
+
+// Questão 02//
+
+let earlyYears = 2; // Esses são os dois primeiros anos da vida de um cão.
+
+// Questão 03//
+
+earlyYears *= 10.5;
+
+// Questão 04//
+
+let laterYears = myAge - 2; 
+
+// Questão 05//
+
+laterYears *= 4; 
+
+// Questão 06//
+
+console.log(earlyYears); 
+console.log(laterYears); 
+
+// Questão 07//
+
+let myAgeInDogYears = earlyYears + laterYears; // Representa minha idade em anos de cão.
+
+// Questão 08//
+
+let myName = "Giovanna Oliveira".toLowerCase(); // Meu nome em letras minúsculas.
+
+// Questão 09//
+
+console.log(`Meu nome é ${myName}. Tenho ${myAge} anos em anos humanos, que são ${myAgeInDogYears} anos em anos de cão.`);
+
+//Desafio Extra//
+const myAge1 = 17;
+let earlyYears1 = 2 * 10.5;
+let laterYears1 = (myAge1 - 2) * 4;
+const myAgeInDogYears1 = earlyYears1 + laterYears1;
+const myName1 = "Giovanna Oliveira".toLowerCase();
+
+console.log(`Meu nome é ${myName1}. Tenho ${myAge1} anos em anos humanos, que são ${myAgeInDogYears1} anos em anos de cão.`);
+
+
+
+
